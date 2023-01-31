a4a068476811957fcd89721063ccb87a3745b8a1
ZaferKok <zaferkok111@gmail.com>
2023-01-31 23:11:02 +0100

    first commit

---
+++ src/test/java/tests/MvnSearchTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MvnSearchTest {
+
+    @Test
+    public void jUnitIleIlkSayfaAcilisi() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://mvnrepository.com/");
+        Thread.sleep(3000);
+
+        WebElement searchBox = driver.findElement(By.xpath("//input[@class='textfield']"));
+        searchBox.sendKeys("Selenium");
+        Thread.sleep(3000);
+
+        WebElement searchButton = driver.findElement(By.xpath("//input[@class='button']"));
+        searchButton.click();
+        Thread.sleep(3000);
+    }
+
+}
